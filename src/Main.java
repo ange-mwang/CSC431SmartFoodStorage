37e2efba0d733eca2491ba74467f15f95329d54c
Alex <alexanderluce@gmail.com>
2022-04-19 00:55:22 -0400

    fix: enable all feats

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,6 @@ import Fridge.Storage;
 class Main {
     public static void main(String[] args) {
         Storage fridge = new Storage();
-        /*
         fridge.initialize();
         fridge.addItem("Milk");
         fridge.addItem("Eggs");
@@ -11,7 +10,6 @@ class Main {
         fridge.addItem("Yogurt");
         fridge.addItem("Asparagus");
         fridge.addItem("Butter");
-        */
         fridge.getRecipes();
     }
 }
